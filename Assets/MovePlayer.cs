88e26b2be33b8e0fd8ea73edf73f6cd85a385fff
lilgamelvr <marti2t5@mail.uc.edu>
2020-09-25 03:04:54 -0400

    Adding the files

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MovePlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    void FixedUpdate()
+    {
+        if (this.isLocalPlayer)
+        {
+            float movement = Input.GetAxis("Horizontal");
+            float movement2 = Input.GetAxis("Vertical");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, movement2 * speed);
+        }
+    }
+}
